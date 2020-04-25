ab38e0515585f72626a3aa64b09844e5e39a2478
Cristina Gassmann <cristina.gassmann@gmx.net>
2020-04-25 11:57:48 +0200

    spring boot setup

---
+++ src/main/java/com/graceandcristina/hackathon/controller/HelloWorldController.java
@@ -0,0 +1,14 @@
+package com.graceandcristina.hackathon.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @RequestMapping("/")
+    public String helloWorld() {
+        return "Hello World!";
+    }
+
+}
