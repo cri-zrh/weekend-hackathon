ce899ce93ebc7315b1b9ef0b8f896a45369e6e97
Cristina Gassmann <cristina.gassmann@gmx.net>
2020-04-25 20:27:05 +0200

    add weekplanner component

---
+++ src/main/resources/frontend/location-app/src/app/weekplanner.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'weekplanner',
+  templateUrl: './weekplanner.component.html',
+  styleUrls: ['./weekplanner.component.css']
+})
+export class WeekplannerComponent {
+
+}
